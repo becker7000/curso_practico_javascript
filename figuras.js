391a8c00067ceab36cbcb51771d383be754f158e
Erick Alvarez <becker7000@outlook.es>
2022-03-29 01:47:52 -0600

    Encapsulando las formulas en funciones

--- figuras.js
+++ figuras.js
@@ -1,79 +1,43 @@
 
-//Variables que vamos a usar para el cuadrado:
-const ladoCuadrado=5;
+//Encapsulando la formula del perímetro del cuadrado en una función: 
+function perimetroCuadrado(lado){
+    return lado*4;
+}
 
-//Agrupando las salidas de la consola según cada figura.
-console.group("---Cuadrado---");
-console.log("Los lados del cuadrado miden: "+ladoCuadrado+" unidades lineales.");
+//Encapsulando la fórmula del área del cuadrado en una función:
+function areaCuadrado(lado){
+    return lado**2;
+}
 
-//Calculando el perímetro del cuadrado: 
-const perimetroCuadrado=ladoCuadrado*4;
-console.log("El perímetro del cuadrado mide: "+perimetroCuadrado+" unidades lineales.");
+//Encapsulando la formula general de Héron para obtener la altura 
+// de un triángulo a tráves de sus lados.
+function alturaTriangulo(lado1, lado2,base){
+    const s=(lado1+lado2+base)/2;
+    const altura = (2/base)*(Math.sqrt(s*(s-lado1)*(s-lado2)*(s-base)));
+    return altura;
+}
 
-//Calculando el área del cuadrado:
-const areaCuadrado=ladoCuadrado**2;
-console.log("El área del cuadrado mide: "+areaCuadrado+" unidades cuadradas.");
+//Encapsulando la formula del perímetro del triángulo en una función.
+function perimetroTriangulo(lado1,lado2,base){
+    return lado1+lado2+base;
+}
 
-console.groupEnd(); //Fin del grupo de salidas de consola del cuadrado.
+//Encapsulando la formula del área del triángulo en una función.
+function areaTriangulo(base,altura){
+    return (base*altura)/2;
+}
 
-
-//Variables que vamos a usar para el triángulo:
-const ladoTriangulo1=5;
-const ladoTriangulo2=5;
-const baseTriangulo=6;
-
-//Agrupando las salidas de consola del triángulo.
-console.group("---Triángulo---");
-console.log("Los lados del triángulo miden: ");
-console.log(" Lado 1: "
-            +ladoTriangulo1
-            +" unidades lineales."
-            +"\n Lado 2: "
-            +ladoTriangulo2
-            +" unidades lineales."
-            +"\n Base: "
-            +baseTriangulo
-            +" unidades lineales."
-            );
-
-//Calculando la altura del triángulo con la fórmula de Herón respecto de la base dada.
-const s=(ladoTriangulo1+ladoTriangulo2+baseTriangulo)/2;
-const alturaTriangulo=(2/baseTriangulo)*(Math.sqrt(s*(s-ladoTriangulo1)*(s-ladoTriangulo2)*(s-baseTriangulo)));
-console.log("La altura del triángulo mide: "+alturaTriangulo+" respecto de la báse.");
-
-//Calculando el perímetro del triángulo.
-const perimetroTriangulo=ladoTriangulo1+ladoTriangulo2+baseTriangulo;
-
-console.log("El perímetro del triángulo mide: "+perimetroTriangulo+" unidades lineales.");
-
-//Calculando el área del triángulo.
-const areaTriangulo=(baseTriangulo*alturaTriangulo)/2;
-console.log("El área del tríangulo mide: "+areaTriangulo+" unidades cuadradas.");
-
-console.groupEnd(); //Fin del grupo de salidas de consola del triángulo.
-
-
-//Variables que vamos a ocupar para el Círculo.
-const radio=4;
-const PI=Math.PI;
-
-//Agrupando las salidas de consola del círculo.
-console.group("---Círculo---");
-console.log("El rádio del círculo mide: "+radio+" unidades lineales.");
-
-//Calculando el diámetro del círculo.
-const diametro=radio*2;
-console.log("El diámetro del círculo mide: "+diametro+" unidades lineales.");
+//Encapsulando la formula del diámetro del círculo en una función.
+function diametroCirculo(radio){
+    return radio*2;
+}
 
 //Calculando el perímetro del círculo.
-const perimetroCirculo=PI*diametro;
-console.log("El perímetro del círculo mide: "+perimetroCirculo+" unidades lineales.");
+function perimetroCirculo(radio){
+    return Math.PI*diametroCirculo(radio);
+}
 
 //Calculando el área del círculo.
-const areaCirculo=PI*Math.pow(radio,2);
-console.log("El área del círculo mide: "+areaCirculo+" unidades cuadradas.");
-
-console.groupEnd(); //Fin del grupo de salidas de consola del círculo.
-
-
-
+function areaCirculo(radio){
+    return Math.PI*Math.pow(radio,2);
+}
